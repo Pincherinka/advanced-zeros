4d4433483c70a830a7bc2fe43e063ee5719dcdd5
tatsiana_shautsova <tatsiana_shautsova@epam.com>
2018-10-04 10:19:11 +0300

    Task 8

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  let result = 0;    
+  let primes = getPrimes(base);
+  for (let key in primes){        
+    if (primes[key] != 0){
+      let resultCandidate = Math.trunc((getCount(number,key)/primes[key]));
+      if (result != 0){
+        result = Math.min(result, resultCandidate)
+      } else result = resultCandidate;      
+    }
+  }
+  return result;
 }
+
+function getPrimes(num) {
+
+  let primes = {};
+  let numTemp = num; 
+  for (let i = 2; i <= numTemp; i++){
+    if (num % i === 0) { 
+      let k = 0;             
+      while (numTemp % i === 0){
+        k++;       
+        numTemp/=i;        
+      }
+       primes[i] = k;        
+    }  
+  } 
+  return primes;
+}
+
+function getCount(number, prime){
+  
+  let i = prime;
+  let count = 0;
+  while (number/i >= 1){    
+    count += Math.floor(number/(i));
+    i *= prime;
+  }
+  return count;
+}
+
